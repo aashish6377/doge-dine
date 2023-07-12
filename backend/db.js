12d6002be0d9ac49f731f0e95a94abeebad3e6d4
aashish6377 <68929785+aashish6377@users.noreply.github.com>
2023-07-13 01:55:33 +0530

    Added User.js and CreateUser.js

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,17 @@
 const mongoose = require("mongoose");
 
 const mongoURI =
-  "mongodb+srv://doge:doge@doge-dine.6i7kopt.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://doge:doge@doge-dine.6i7kopt.mongodb.net/Food?retryWrites=true&w=majority";
 
 const mongoDb = async () => {
   try {
     await mongoose.connect(mongoURI);
     console.log("Connected to database");
+
+    const fetchedData = await mongoose.connection.db.collection("food-items");
+    const data = await fetchedData.find({}).toArray();
+
+    // console.log(data);
   } catch (error) {
     console.error("Error connecting to database:", error);
   }
