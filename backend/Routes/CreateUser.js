970f71354e53b0288bf3d0652e1dfc48e30a0e2b
aashish6377 <68929785+aashish6377@users.noreply.github.com>
2023-07-13 13:25:34 +0530

    Created Sign Up and Login

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -1,20 +1,66 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
-router.post("/createUser", async (req, res) => {
-  try {
-    await User.create({
-      name: req.body.name,
-      location: req.body.location,
-      email: req.body.email,
-      password: req.body.password,
-    });
-    res.json({ success: true });
-  } catch (error) {
-    console.log(error);
-    res.json({ success: false });
+router.post(
+  "/createuser",
+  [
+    body("email").isEmail(),
+    body("name").isLength({ min: 5 }),
+    body("password", "Password not suitable").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      await User.create({
+        name: req.body.name,
+        location: req.body.location,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      res.json({ success: true });
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false });
+    }
   }
-});
+);
+
+router.post(
+  "/login",
+  [
+    body("email").isEmail(),
+    body("password", "Password length invalid").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    let email = req.body.email;
+    try {
+      let userData = await User.findOne({ email });
+      if (!userData) {
+        return res
+          .status(400)
+          .json({ errors: "Try logging in with correct credentials" });
+      }
+      if (req.body.password !== userData.password) {
+        return res
+          .status(400)
+          .json({ errors: "Try logging in with correct credentials" });
+      }
+      return res.json({ success: true });
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false });
+    }
+  }
+);
 
 module.exports = router;
